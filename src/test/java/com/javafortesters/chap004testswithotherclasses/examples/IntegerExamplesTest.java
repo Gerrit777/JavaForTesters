8c7e9a111a2c92ffd24c277bcdd5eadc890bf40e
Gerrit Brands <g.brands1@chello.nl>
2020-12-31 14:44:55 +0100

    Chapter 4 -- Exercise convert Integer into Hex

--- src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
+++ src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
@@ -16,5 +16,18 @@ public class IntegerExamplesTest {
 
         Integer six = 6;
         assertEquals("autoboxing assignment for 6",6,six.intValue());
+
+        int y = Integer.valueOf("7");
+        int x = Integer.parseInt("8");
+        int z = x+y;
+        System.out.println("x + y ="+z);
+    }
+
+    @Test
+    public void convertAnIntToHex() {
+        assertEquals("that 11 becomes b","b",Integer.toHexString(11));
+        assertEquals("that 10 becomes a","a",Integer.toHexString(10));
+        assertEquals("that 11 becomes 3","3",Integer.toHexString(3));
+        assertEquals("that 11 becomes 15","15",Integer.toHexString(21));
     }
 }
