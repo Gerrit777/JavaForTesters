7774af6e387719681b6946bef9fd2d98257f374b
Gerrit Brands <g.brands1@chello.nl>
2021-01-04 18:25:38 +0100

    Chapter 5 -- Class TestAppEnvironmentTest

---
+++ src/test/java/com/javafortesters/chap005testwithourownclasses/domainobject/examples/TestAppEnvironmentTest.java
@@ -0,0 +1,11 @@
+package com.javafortesters.chap005testwithourownclasses.domainobject.examples;
+
+import org.junit.Test;
+
+public class TestAppEnvironmentTest {
+
+    @Test
+    public void canGetUrlStatically(){
+
+    }
+}
