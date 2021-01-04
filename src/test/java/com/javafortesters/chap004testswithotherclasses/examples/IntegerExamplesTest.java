6ad0adb58a449f93cc907752109798de09852323
Gerrit Brands <g.brands1@chello.nl>
2021-01-04 17:32:47 +0100

    Chapter 4 -- Exercise Check Max and Min Integer

--- src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
+++ src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
@@ -9,25 +9,33 @@ public class IntegerExamplesTest {
     @Test
     public void integerExploration() {
         Integer four = new Integer(4);
-        assertEquals("intValue returns int 4",4,four.intValue());
+        assertEquals("intValue returns int 4", 4, four.intValue());
 
         Integer five = new Integer("5");
-        assertEquals("intValue returns int 5",5,five.intValue());
+        assertEquals("intValue returns int 5", 5, five.intValue());
 
         Integer six = 6;
-        assertEquals("autoboxing assignment for 6",6,six.intValue());
+        assertEquals("autoboxing assignment for 6", 6, six.intValue());
 
         int y = Integer.valueOf("7");
         int x = Integer.parseInt("8");
-        int z = x+y;
-        System.out.println("x + y ="+z);
+        int z = x + y;
+        System.out.println("x + y =" + z);
     }
 
     @Test
     public void convertAnIntToHex() {
-        assertEquals("that 11 becomes b","b",Integer.toHexString(11));
-        assertEquals("that 10 becomes a","a",Integer.toHexString(10));
-        assertEquals("that 11 becomes 3","3",Integer.toHexString(3));
-        assertEquals("that 11 becomes 15","15",Integer.toHexString(21));
+        assertEquals("that 11 becomes b", "b", Integer.toHexString(11));
+        assertEquals("that 10 becomes a", "a", Integer.toHexString(10));
+        assertEquals("that 11 becomes 3", "3", Integer.toHexString(3));
+        assertEquals("that 11 becomes 15", "15", Integer.toHexString(21));
+    }
+
+    @Test
+    public void confirmMaxAndMinIntegerSizes() {
+        int minimumInt = -2147483648;
+        int maximumInt = 2147483647;
+        assertEquals("Check MAX integer size", maximumInt, Integer.MAX_VALUE);
+        assertEquals("Check MIN integer size",minimumInt, Integer.MIN_VALUE);
     }
 }
