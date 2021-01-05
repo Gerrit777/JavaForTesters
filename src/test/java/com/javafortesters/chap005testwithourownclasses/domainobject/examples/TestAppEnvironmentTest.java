788f281423a2ee82bd240da105b1414b841cee0a
Gerrit Brands <g.brands1@chello.nl>
2021-01-05 17:26:35 +0100

    Chapter 5 -- Class TestAppEnvironmentTest + TestAppEnv

--- src/test/java/com/javafortesters/chap005testwithourownclasses/domainobject/examples/TestAppEnvironmentTest.java
+++ src/test/java/com/javafortesters/chap005testwithourownclasses/domainobject/examples/TestAppEnvironmentTest.java
@@ -1,11 +1,19 @@
 package com.javafortesters.chap005testwithourownclasses.domainobject.examples;
 
+import static com.javafortesters.domainobject.TestAppEnv.*;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestAppEnvironmentTest {
 
     @Test
     public void canGetUrlStatically(){
+        assertEquals("Returns Hard Coded URL","http://192.123.0.3:67","http://"+DOMAIN+":"+PORT);
+    }
 
+    @Test
+    public void canGetDomainAndPortStatically(){
+        assertEquals("Just the Domain","192.123.0.3",DOMAIN);
+        assertEquals("Just the port","67",PORT);
     }
 }
