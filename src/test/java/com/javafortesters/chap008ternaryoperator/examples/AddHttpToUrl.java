2d327b2d5061478ecc2df3b0eb457b7a4a232b39
Gerrit Brands <g.brands1@chello.nl>
2021-01-07 17:08:07 +0100

    Chapter 8 : AssertTrue exercise (added 1 extra @Test methods) final version

---
+++ src/test/java/com/javafortesters/chap008ternaryoperator/examples/AddHttpToUrl.java
@@ -0,0 +1,48 @@
+package com.javafortesters.chap008ternaryoperator.examples;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddHttpToUrl {
+    @Test
+    public void ifAddHttp(){
+        String url = "www.seleniumsimplified.com"; if(!url.startsWith("http")){
+            url = addHttp(url);
+        }
+        assertTrue(url.startsWith("http://"));
+        assertEquals("http://www.seleniumsimplified.com", url);
+    }
+
+    private String addHttp(String url) {
+        return "http://"+url;
+    }
+
+    // Exercise page 117 (chapter 8)
+    @Test
+        public void assertIfTruePart1(){
+        boolean truthy=true;
+        if (truthy)
+                assertTrue(truthy);
+    }
+    @Test
+    public void assertIfTruePart2(){
+        boolean truthy=true;
+        if (truthy) {
+            assertTrue(truthy);
+            assertFalse(!truthy);
+        }
+    }
+    @Test
+    public void assertIfTruePart3(){
+        boolean truthy=true;
+        if (truthy) {
+            assertTrue(truthy);
+            assertFalse(!truthy);
+        }
+        else
+            assertFalse(truthy);
+        }
+    }
+
+
